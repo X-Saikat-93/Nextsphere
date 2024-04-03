fccfb84a6375c3ff47c10a86541f7e82907fe6a0
Saikat Das <dassaikat1901@gmail.com>
2024-04-03 08:04:51 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" m-auto w-auto">
+      <Image src="/next.svg" alt="Next.js Logo" width={180} height={37} />
+     <h1> This is going to be a Massive project with full Authentication & End to End TypeSafe. </h1>
+    </main>
+  );
+}
