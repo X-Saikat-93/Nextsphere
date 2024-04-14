27c4c443919f0534c40a7fe96ca7e37aaab72e23
Saikat Das <dassaikat1901@gmail.com>
2024-04-14 13:19:46 +0530

    Sidebar configure

---
+++ src/components/mobile-sidebar.tsx
@@ -0,0 +1,17 @@
+import { Menu,SquareMenu  } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sidebar } from "@/components/sidebar";
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <SquareMenu size={20} className='text-white' />
+      </SheetTrigger>
+      <SheetContent className='p-0 z-[100]' side='left'>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
