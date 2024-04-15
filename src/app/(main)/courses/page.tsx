142b38d932fa1a64635beafe9e6e20a9b6e58f05
Saikat Das <dassaikat1901@gmail.com>
2024-04-15 16:18:59 +0530

    UI card style changes.

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -1,10 +1,10 @@
-import { getCourses, getUserProgress, } from "../../../../db/quaries";
+import { getCourses, getUserProgress } from "../../../../db/quaries";
 
 import { List } from "./list";
 const CoursesPage = async () => {
   const coursesData = getCourses();
   //   const userProgressData = getUserProgress();
-  const userProgressData =  getUserProgress();
+  const userProgressData = getUserProgress();
 
   const [courses, userProgress] = await Promise.all([
     coursesData,
@@ -13,9 +13,8 @@ const CoursesPage = async () => {
 
   return (
     <div className='h-full max-w-[912px] px-3 mx-auto'>
-      <h1 className='text-2xl font-bold text-neutral-700'>Language Courses</h1>
-          <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
-         
+      <h1 className='text-2xl font-bold text-current lg:text-3xl text-center'>Language Courses</h1>
+      <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
   );
 };
