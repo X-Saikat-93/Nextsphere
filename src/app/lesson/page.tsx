5921a44e55306649a1011c5489edca72c2844175
Saikat Das <dassaikat1901@gmail.com>
2024-04-16 21:28:44 +0530

    Lesson Page Updated

--- src/app/lesson/page.tsx
+++ src/app/lesson/page.tsx
@@ -2,35 +2,34 @@ import { redirect } from "next/navigation";
 import { getLesson, getUserProgress } from "../../../db/quaries";
 import Quiz from "./quiz";
 const LessonPage = async () => {
+  const lessonData = getLesson();
+  const userProgressData = getUserProgress();
+  // const userSubscriptionData = getUserSubscription();
 
-    const lessonData = getLesson();
-    const userProgressData = getUserProgress();
-    
-    const [lesson, userProgress] = await Promise.all([
-      lessonData,
-      userProgressData,
-      
-    ]);
+  const [lesson, userProgress] = await Promise.all([
+    lessonData,
+    userProgressData,
+    // userSubscriptionData,
+  ]);
 
-    if (!lesson || !userProgress) { 
-         redirect("/learn");
-    };
+  if (!lesson || !userProgress) {
+    redirect("/learn");
+  }
 
-     const initialPercentage =
-       (lesson.challenges.filter((challenge) => challenge.completed).length /
-         lesson.challenges.length) *
-       100;
-    return (
-      <>
-        <Quiz
-          initialLessonId={lesson.id}
-          initialLessonChallenges={lesson.challenges}
-          initialHearts={userProgress.hearts}
-          initialPercentage={initialPercentage}
-          userSubscription={UserSubscription}
-        />
-      </>
-    );
+  const initialPercentage =
+    (lesson.challenges.filter((challenge) => challenge.completed).length /
+      lesson.challenges.length) *
+    100;
+
+  return (
+    <Quiz
+      initialLessonId={lesson.id}
+      initialLessonChallenges={lesson.challenges}
+      initialHearts={userProgress.hearts}
+      initialPercentage={initialPercentage}
+      userSubscription={undefined}
+    />
+  );
 };
 
 export default LessonPage;
--- src/app/lesson/quiz.tsx
+++ src/app/lesson/quiz.tsx
@@ -1,5 +1,9 @@
 "use client";
+
 import { challenges, challengeOptions } from "../../../db/schema";
+import { useState, useTransition } from "react";
+import Confetti from "react-confetti";
+import { Header } from "./header";
 type Props = {
   initialPercentage: number;
   initialHearts: number;
@@ -18,11 +22,27 @@ const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+    const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(() => {
+    return initialPercentage === 100 ? 0 : initialPercentage;
+  });
     
     return (
-      
-        
-  );
+      <>
+        <Confetti
+          recycle={false}
+          numberOfPieces={500}
+          tweenDuration={10000}
+          width={50}
+          height={50}
+        />
+        <Header
+          hearts={hearts}
+          percentage={50}
+          hasActiveSubscription={!!userSubscription?.isActive}
+        />
+      </>
+    );
 };
 
 export default Quiz;
