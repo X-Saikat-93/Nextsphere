ddfeeb0d76f7b25437850824a2cdb132dfb144dc
Saikat Das <dassaikat1901@gmail.com>
2024-04-17 06:27:30 +0530

    Quiz page questions issue resolved

--- src/app/lesson/quiz.tsx
+++ src/app/lesson/quiz.tsx
@@ -4,6 +4,8 @@ import { challenges, challengeOptions } from "../../../db/schema";
 import { useState, useTransition } from "react";
 import Confetti from "react-confetti";
 import { Header } from "./header";
+import { QuestionBubble } from "./question-bubble";
+import { Challeneg } from "./challenge";
 type Props = {
   initialPercentage: number;
   initialHearts: number;
@@ -22,13 +24,36 @@ const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+    const [pending, startTransition] = useTransition();
   const [hearts, setHearts] = useState(initialHearts);
+
   const [percentage, setPercentage] = useState(() => {
     return initialPercentage === 100 ? 0 : initialPercentage;
   });
+  const [challenges] = useState(initialLessonChallenges);
+  const [lessonId, setLessonId] = useState(initialLessonId);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+  const challenge = challenges[activeIndex];
 
+   const [selectedoption, setSelectedOption] = useState<number>();
+  const options = challenge?.challengeOptions ?? [];
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
 
-  
+  const onSelect = (id: number) => {
+    if (status != "none") return;
+
+    setSelectedOption(id);
+  };
+
   return (
     <>
       <Confetti
@@ -47,9 +72,9 @@ const Quiz = ({
         <div className='h-full flex items-center justify-center'>
           <div className='lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12'>
             <h1 className='text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700'>
-            Heading
+              {title}
             </h1>
-            {/* <div>
+            <div>
               {challenge.type === "ASSIST" && (
                 <QuestionBubble question={challenge.question} />
               )}
@@ -61,7 +86,7 @@ const Quiz = ({
                 disabled={pending}
                 type={challenge.type}
               />
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
